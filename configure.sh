a4ce089d3a4300dcc5b620f1eee8ace2d6681787
Stefan Altmayer <Stoeoef@gmail.com>
2013-10-25 00:15:08 +0200

    Configure script won't warn if deps directory already exist

--- configure.sh
+++ configure.sh
@@ -1,8 +1,12 @@
 
 # make or-tools
-mkdir deps
+mkdir -p deps
 cd deps
 svn checkout http://or-tools.googlecode.com/svn/trunk/ or-tools-read-only
 cd or-tools-read-only
 make third_party
 make cplibs
+# remove shared objects to enforce static linking
+rm -v lib/*.so lib/*.so.*
+cd dependencies/install/lib
+rm -v *.so *.so.*
